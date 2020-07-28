09c396e1a104847f4f776b00142e6aad9b9dca78
TMTY <salaudeen.t@gmail.com>
2020-07-28 23:53:13 +0100

    file structure

---
+++ js/firebaseConfig.js
@@ -0,0 +1,15 @@
+
+// Your web app's Firebase configuration
+var firebaseConfig = {
+    apiKey: "AIzaSyCW4bi_m1aDkdzr2yqkMCHgVkSSF84jba4",
+    authDomain: "quiz-app-tmty.firebaseapp.com",
+    databaseURL: "https://quiz-app-tmty.firebaseio.com",
+    projectId: "quiz-app-tmty",
+    storageBucket: "quiz-app-tmty.appspot.com",
+    messagingSenderId: "495006867216",
+    appId: "1:495006867216:web:91bd6c4ab8821c0707cf95",
+    measurementId: "G-YT4K8E2324"
+};
+
+// Initialize Firebase
+firebase.initializeApp(firebaseConfig);
